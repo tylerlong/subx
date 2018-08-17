74b04b4a8abb4ed2bdb5180afd322fb2ab32bf5e
Tyler Long <tyler4long@gmail.com>
2018-08-17 16:47:39 +0800

    Going to release it

---
+++ webpack.config.babel.js
@@ -0,0 +1,18 @@
+export default {
+  mode: 'production',
+  devtool: 'source-map',
+  entry: './src/index.js',
+  output: {
+    library: 'ReactiveModel',
+    libraryTarget: 'umd',
+    globalObject: 'this' // fix window undefined issue in node
+  },
+  externals: {
+    rxjs: {
+      commonjs: 'rxjs',
+      commonjs2: 'rxjs',
+      amd: 'rxjs',
+      root: 'rxjs'
+    }
+  }
+}
