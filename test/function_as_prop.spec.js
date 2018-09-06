41eebbf6680781b795fabc114d69255d83c07a84
Tyler Long <tyler4long@gmail.com>
2018-09-06 17:00:30 +0800

    babel-core@7.0.0-bridge.0

--- test/function_as_prop.spec.js
+++ test/function_as_prop.spec.js
@@ -1,6 +1,21 @@
 /* eslint-env jest */
+import SubX from '../src/index'
+
 describe('function as prop', () => {
   test('uuid', () => {
-
+    const Person = SubX({
+      firstName: '',
+      lastName: ''
+    })
+    const person1 = new Person({
+      firstName: 'San',
+      lastName: 'Zhang'
+    })
+    const person2 = new Person({
+      firstName: 'Si',
+      lastName: 'Li'
+    })
+    expect(person1.firstName).toBe('San')
+    expect(person2.lastName).toBe('Li')
   })
 })
