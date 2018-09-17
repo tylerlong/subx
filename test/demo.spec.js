4b490f0c786ad8d4b83887394803feb345d85629
Tyler Long <tyler4long@gmail.com>
2018-09-17 22:24:59 +0800

    Fix demo.spec.js

--- test/demo.spec.js
+++ test/demo.spec.js
@@ -1,9 +1,11 @@
 /* eslint-env jest */
+import { filter } from 'rxjs/operators'
+
 import SubX from '../src/index'
 
 describe('demo', () => {
   test('props changed events', () => {
-    const Person = SubX({
+    const Person = new SubX({
       firstName: '',
       lastName: ''
     })
@@ -11,10 +13,10 @@ describe('demo', () => {
       firstName: 'San',
       lastName: 'Zhang'
     })
-    person.firstName$.subscribe(mutation => {
+    person.$.pipe(filter(action => action.prop === 'firstName')).subscribe(mutation => {
       console.log('First name changed', mutation)
     })
-    person.lastName$.subscribe(mutation => {
+    person.$.pipe(filter(action => action.prop === 'lastName')).subscribe(mutation => {
       console.log('Last name changed', mutation)
     })
     person.firstName = 'Si'
@@ -23,18 +25,18 @@ describe('demo', () => {
     person.firstName = 'Wu'
   })
 
-  // test('Subscribe to all the props changed events', () => {
-  //   const Person = SubX({
-  //     firstName: 'San',
-  //     lastName: 'Zhang'
-  //   })
-  //   const person = new Person()
-  //   person.$.subscribe(mutation => {
-  //     console.log('Prop changed', mutation)
-  //   })
-  //   person.firstName = 'Si'
-  //   person.lastName = 'Li'
-  //   person.lastName = 'Wang'
-  //   person.firstName = 'Wu'
-  // })
+  test('Subscribe to all the props changed events', () => {
+    const Person = new SubX({
+      firstName: 'San',
+      lastName: 'Zhang'
+    })
+    const person = new Person()
+    person.$.subscribe(mutation => {
+      console.log('Prop changed', mutation)
+    })
+    person.firstName = 'Si'
+    person.lastName = 'Li'
+    person.lastName = 'Wang'
+    person.firstName = 'Wu'
+  })
 })
