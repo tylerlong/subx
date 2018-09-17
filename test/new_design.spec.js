d12f0aff676371084e87cd84da8243e755e3ce76
Tyler Long <tyler4long@gmail.com>
2018-09-17 15:24:57 +0800

    Computed property

--- test/new_design.spec.js
+++ test/new_design.spec.js
@@ -48,62 +48,69 @@ const SubX = {
 }
 
 describe('new design', () => {
-  test('prototype', () => {
-    const p = SubX.create({ hello: 'world' })
+  // test('prototype', () => {
+  //   const p = SubX.create({ hello: 'world' })
 
-    p.$.subscribe(action => {
-      console.log('1:', action)
-    })
-    p.firstName = 'Si'
-    p.lastName = 'Li'
+  //   p.$.subscribe(action => {
+  //     console.log('1:', action)
+  //   })
+  //   p.firstName = 'Si'
+  //   p.lastName = 'Li'
 
-    p.$.subscribe(action => {
-      console.log('2:', action)
-    })
-    p.$$.subscribe(action => {
-      console.log('3:', action)
-    })
-    p.firstName = 'Wu'
-    p.lastName = 'Wang'
+  //   p.$.subscribe(action => {
+  //     console.log('2:', action)
+  //   })
+  //   p.$$.subscribe(action => {
+  //     console.log('3:', action)
+  //   })
+  //   p.firstName = 'Wu'
+  //   p.lastName = 'Wang'
 
-    console.log(JSON.stringify(p, null, 2))
-  })
+  //   console.log(JSON.stringify(p, null, 2))
+  // })
 
-  test('array', () => {
-    const a = SubX.create([])
-    a.$.subscribe(action => {
-      console.log(action)
-    })
-    a.push(1)
-    a.push(2)
-    a[1] = 3
-    a.unshift()
-  })
+  // test('array', () => {
+  //   const a = SubX.create([])
+  //   a.$.subscribe(action => {
+  //     console.log(action)
+  //   })
+  //   a.push(1)
+  //   a.push(2)
+  //   a[1] = 3
+  //   a.unshift()
+  // })
 
-  test('nested', () => {
-    const n = SubX.create({ a: { } })
-    n.$.subscribe(action => {
-      console.log(action)
-    })
-    n.a.$.subscribe(action => {
-      console.log(action)
-    })
-    n.a.b = 'hello'
-  })
+  // test('nested', () => {
+  //   const n = SubX.create({ a: { } })
+  //   n.$.subscribe(action => {
+  //     console.log(action)
+  //   })
+  //   n.a.$.subscribe(action => {
+  //     console.log(action)
+  //   })
+  //   n.a.b = 'hello'
+  // })
 
-  test('$$', () => {
-    const n = SubX.create({ a: { } })
-    n.$$.subscribe(action => {
-      console.log(action)
-    })
-    n.a.b = {}
-    n.a.b.c = {}
-    n.a.b.c.$$.subscribe(action => {
-      console.log(action)
-    })
-    n.a.b.c.d = {}
-    n.a.b.c.d.e = {}
-    console.log(n)
-    console.log(n + '')
+  // test('$$', () => {
+  //   const n = SubX.create({ a: { } })
+  //   n.$$.subscribe(action => {
+  //     console.log(action)
+  //   })
+  //   n.a.b = {}
+  //   n.a.b.c = {}
+  //   n.a.b.c.$$.subscribe(action => {
+  //     console.log(action)
+  //   })
+  //   n.a.b.c.d = {}
+  //   n.a.b.c.d.e = {}
+  //   console.log(n)
+  //   console.log(n + '')
+  // })
+
+  test('computed property', () => {
+    const p = SubX.create({ firstName: '', lastName: '', fullName: function () { return `${this.firstName} ${this.lastName}` } })
+    p.firstName = 'Chuntao'
+    p.lastName = 'Liu'
+    console.log(p.fullName())
   })
 })
