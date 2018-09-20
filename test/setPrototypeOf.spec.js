85d0be562743e7a901d7425e639fbda737df7f64
Tyler Long <tyler4long@gmail.com>
2018-09-20 21:12:25 +0800

    Add a test case to show why should not allow setPrototypeOf

--- test/setPrototypeOf.spec.js
+++ test/setPrototypeOf.spec.js
@@ -38,4 +38,23 @@ describe('setPrototypeOf', () => {
     const f = () => Object.setPrototypeOf(p, { b: 1 })
     expect(f).toThrow(TypeError)
   })
+  test('too flexible', () => {
+    let count = 0
+    const o = {}
+    const handler = {
+      set: (target, prop, val, receiver) => {
+        target[prop] = val
+        count += 1
+        return true
+      }
+    }
+    const p = new Proxy(o, handler)
+    p.a = 1 // trigger handler.set
+    expect(count).toBe(1)
+    const b = {}
+    Object.setPrototypeOf(p, b)
+    b.c = 2 // doesn't trigget handler.set
+    expect(count).toBe(1) // still 1
+    expect(p.c).toBe(2)
+  })
 })
