528f698807aa1e040e39657545970cb97cc77c70
Tyler Long <tyler4long@gmail.com>
2018-09-21 18:05:08 +0800

    Add test for sub streams

---
+++ test/sub_streams.spec.js
@@ -0,0 +1,77 @@
+/* eslint-env jest */
+import SubX from '../src/index'
+import * as R from 'ramda'
+
+describe('sub streams', () => {
+  test('set$', () => {
+    const p = SubX.create()
+    const events = []
+    p.set$.subscribe(event => {
+      events.push(event)
+    })
+    p.a = 1
+    p.b = 2
+    p.a = 3
+    delete p.b
+    expect(events).toEqual([
+      {
+        type: 'SET',
+        path: ['a'],
+        val: 1,
+        oldVal: undefined
+      },
+      {
+        type: 'SET',
+        path: ['b'],
+        val: 2,
+        oldVal: undefined
+      },
+      {
+        type: 'SET',
+        path: ['a'],
+        val: 3,
+        oldVal: 1
+      }
+    ])
+  })
+
+  test('delete$', () => {
+    const p = SubX.create()
+    const events = []
+    p.delete$.subscribe(event => {
+      events.push(event)
+    })
+    p.a = 1
+    p.b = 2
+    p.a = 3
+    delete p.b
+    expect(events).toEqual([
+      {
+        type: 'DELETE',
+        path: ['b'],
+        val: 2
+      }
+    ])
+  })
+
+  test('$', () => {
+    const p = SubX.create()
+    const events1 = []
+    const events2 = []
+    const events3 = []
+    p.set$.subscribe(event => {
+      events1.push(event)
+    })
+    p.delete$.subscribe(event => {
+      events2.push(event)
+    })
+    p.$.subscribe(event => {
+      events3.push(event)
+    })
+    p.a = 1
+    p.b = 2
+    p.a = 3
+    delete p.b
+    expect(events3).toEqual(R.concat(events1, events2))
+  })
+})
