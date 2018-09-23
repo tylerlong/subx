29172d7921ff3925373b09a9628435572724e481
Tyler Long <tyler4long@gmail.com>
2018-09-23 20:49:53 +0800

    Test defineProperty override

--- test/defineProperty.spec.js
+++ test/defineProperty.spec.js
@@ -21,6 +21,24 @@ describe('defineProperty', () => {
     expect(p.a).toBe(3)
     expect(count).toBe(1) // defineProperty doesn't trigger handler.set
   })
+  test('override', () => {
+    const o = {}
+    let count = 0
+    const handler = {
+      set: (target, prop, val, receiver) => {
+        count += 1
+        target[prop] = val
+        return true
+      }
+    }
+    const p = new Proxy(o, handler)
+    p.a = 1
+    expect(count).toBe(1)
+    Object.defineProperty(p, 'a', { value: 2 })
+    Object.defineProperty(p, 'a', { value: 3 })
+    expect(p.a).toBe(3)
+    expect(count).toBe(1) // defineProperty doesn't trigger handler.set
+  })
   test('subX', () => {
     const p = SubX.create()
     const f = () => Object.defineProperty(p, 'a', { value: 1 })
