638e2f0d5664240203638db3afe225d52a9cc738
Tyler Long <tyler4long@gmail.com>
2018-10-05 20:47:16 +0800

    Add a lazy stream test case

---
+++ test/lazy_stream.spec.js
@@ -0,0 +1,112 @@
+/* eslint-env jest */
+import { filter, map, publish } from 'rxjs/operators'
+import { Subject } from 'rxjs'
+
+import SubX from '../src/index'
+
+describe('lazy stream', () => {
+  test('no subscriber', () => {
+    const p = SubX.create({})
+    let count = 0
+    const lazyStream = p.$.pipe(
+      filter(event => {
+        count += 1
+        return true
+      }),
+      map(event => {
+        count += 1
+        return event
+      })
+    )
+    expect(lazyStream).toBeDefined()
+    p.firstName = 'Tyler'
+    p.lastName = 'Liu'
+    expect(count).toBe(0) // because no subscriber.
+  })
+
+  test('late subscriber', () => {
+    const p = SubX.create({})
+    let count = 0
+    const lazyStream = p.$.pipe(
+      filter(event => {
+        count += 1
+        return true
+      }),
+      map(event => {
+        count += 1
+        return event
+      })
+    )
+    expect(lazyStream).toBeDefined()
+    p.firstName = 'Tyler'
+    p.lastName = 'Liu'
+    lazyStream.subscribe(event => {})
+    expect(count).toBe(0) // because subscriber too late
+  })
+
+  test('single subscriber', () => {
+    const p = SubX.create({})
+    let count = 0
+    const lazyStream = p.$.pipe(
+      filter(event => {
+        count += 1
+        return true
+      }),
+      map(event => {
+        count += 1
+        return event
+      })
+    )
+    expect(lazyStream).toBeDefined()
+    lazyStream.subscribe(event => {})
+    p.firstName = 'Tyler'
+    p.lastName = 'Liu'
+    expect(count).toBe(4)
+  })
+
+  test('mutiple subscribers', () => {
+    const p = SubX.create({})
+    let count = 0
+    const lazyStream = p.$.pipe(
+      filter(event => {
+        count += 1
+        return true
+      }),
+      map(event => {
+        count += 1
+        return event
+      }),
+      publish()
+    ).refCount()
+    expect(lazyStream).toBeDefined()
+    lazyStream.subscribe(event => {})
+    lazyStream.subscribe(event => {})
+    lazyStream.subscribe(event => {})
+    p.firstName = 'Tyler'
+    p.lastName = 'Liu'
+    expect(count).toBe(4) // this is weird!
+  })
+
+  test('rxjs subject', () => {
+    const p = new Subject()
+    let count = 0
+    const lazyStream = p.pipe(
+      filter(event => {
+        count += 1
+        return true
+      }),
+      map(event => {
+        count += 1
+        return event
+      }),
+      publish()
+    ).refCount()
+    expect(lazyStream).toBeDefined()
+    lazyStream.subscribe(event => {})
+    lazyStream.subscribe(event => {})
+    lazyStream.subscribe(event => {})
+    p.next('Tyler')
+    p.next('Liu')
+    expect(count).toBe(4) // this is weird!
+  })
+})
