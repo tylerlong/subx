150c4406d4e4d339d063e8786c9b32d59af49ef9
Tyler Long <tyler4long@gmail.com>
2018-10-06 15:37:21 +0800

    More test case for array

--- test/array_splice.spec.js
+++ test/array_splice.spec.js
@@ -7,7 +7,9 @@ describe('array splice', () => {
       todos: [1, 2, 3]
     })
     let notified = false
-    store.$.subscribe(event => { notified = true })
+    store.$.subscribe(event => {
+      notified = true
+    })
     store.todos.push(4)
     expect(notified).toBeTruthy()
     notified = false
@@ -17,5 +19,25 @@ describe('array splice', () => {
     expect(store.todos.length).toBe(3)
     store.todos.push(5)
     expect(notified).toBeTruthy()
+  })
+  test('number of notifications', () => {
+    const store = SubX.create({
+      todos: [1, 2, 3]
+    })
+    const events = []
+    store.$.subscribe(event => events.push(event))
+    store.todos.splice(1, 1)
+    expect(events[events.length - 1]).toEqual({ type: 'SET', path: [ 'todos', 'length' ], val: 2, oldVal: 3 })
+    expect(events[events.length - 2].type).toBe('DELETE')
+  })
+  test('shift', () => {
+    const store = SubX.create({
+      todos: [1, 2, 3]
+    })
+    const events = []
+    store.$.subscribe(event => events.push(event))
+    store.todos.shift(0)
+    expect(events[events.length - 1]).toEqual({ type: 'SET', path: [ 'todos', 'length' ], val: 2, oldVal: 3 })
+    expect(events[events.length - 2].type).toBe('DELETE')
   })
 })
