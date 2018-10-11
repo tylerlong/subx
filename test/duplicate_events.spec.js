ef2c8c4028328fad9a2ba3d4b81ec0e331a4863e
Tyler Long <tyler4long@gmail.com>
2018-10-11 18:01:06 +0800

    Find a way to remove duplicate events

--- test/duplicate_events.spec.js
+++ test/duplicate_events.spec.js
@@ -20,12 +20,10 @@ describe('duplicate events', () => {
         }
       ]
     })
-
     const todo = store.todos[1]
-
     const props = SubX.create({ store, todo })
 
-    const events = []
+    let events = []
     props.get$.subscribe(e => events.push(e))
     expect(todo.title).toBe('222')
     expect(R.map(R.dissoc('id'), events)).toEqual([
@@ -37,6 +35,189 @@ describe('duplicate events', () => {
       }
     ])
     expect(events[0].id).toBe(events[1].id)
+
+    events = []
+    todo.a = { b: { c: { d: 'hello' } } }
+    expect(todo.a.b.c.d).toBe('hello')
+    expect(R.map(R.dissoc('id'), events)).toEqual(R.map(R.dissoc('id'), [
+      { type: 'GET',
+        path: [ 'store', 'todos', '1', 'a' ],
+        id: 'cdd804c0-49a0-4ca3-8623-5dd2383d337a' },
+      { type: 'GET',
+        path: [ 'todo', 'a' ],
+        id: 'cdd804c0-49a0-4ca3-8623-5dd2383d337a' },
+      { type: 'GET',
+        path: [ 'store', 'todos', '1', 'a', 'b' ],
+        id: '0b0dce92-31a9-40fa-8865-b71d000a05d8' },
+      { type: 'GET',
+        path: [ 'todo', 'a', 'b' ],
+        id: '0b0dce92-31a9-40fa-8865-b71d000a05d8' },
+      { type: 'GET',
+        path: [ 'store', 'todos', '1', 'a', 'b', 'c' ],
+        id: '15de561b-0ec4-448c-825c-d5fb087d8a9b' },
+      { type: 'GET',
+        path: [ 'todo', 'a', 'b', 'c' ],
+        id: '15de561b-0ec4-448c-825c-d5fb087d8a9b' },
+      { type: 'GET',
+        path: [ 'store', 'todos', '1', 'a', 'b', 'c', 'd' ],
+        id: '6ff7380e-1088-4637-b8be-dfe0d1ab5701' },
+      { type: 'GET',
+        path: [ 'todo', 'a', 'b', 'c', 'd' ],
+        id: '6ff7380e-1088-4637-b8be-dfe0d1ab5701' }
+    ]))
+    expect(events[0].id).toBe(events[1].id)
+    expect(events[2].id).toBe(events[3].id)
+    expect(events[4].id).toBe(events[5].id)
+    expect(events[6].id).toBe(events[7].id)
+
+    events = []
+    expect(store.todos[1].a.b.c.d).toBe('hello')
+    expect(R.map(R.dissoc('id'), events)).toEqual(R.map(R.dissoc('id'), [
+      { type: 'GET',
+        path: [ 'store', 'todos' ],
+        id: 'ccd367ca-fd6e-4400-ab3d-039309ca881f' },
+      { type: 'GET',
+        path: [ 'store', 'todos', '1' ],
+        id: 'bf56cbcb-9d56-4f59-9ea3-4cbcca5d0c9f' },
+      { type: 'GET',
+        path: [ 'store', 'todos', '1', 'a' ],
+        id: '0f10e195-14d1-49a0-99f4-5bc5e7a38108' },
+      { type: 'GET',
+        path: [ 'todo', 'a' ],
+        id: '0f10e195-14d1-49a0-99f4-5bc5e7a38108' },
+      { type: 'GET',
+        path: [ 'store', 'todos', '1', 'a', 'b' ],
+        id: '8682c81f-582b-437b-85cd-7fba0bbea26e' },
+      { type: 'GET',
+        path: [ 'todo', 'a', 'b' ],
+        id: '8682c81f-582b-437b-85cd-7fba0bbea26e' },
+      { type: 'GET',
+        path: [ 'store', 'todos', '1', 'a', 'b', 'c' ],
+        id: '737017b2-6d95-4918-a64e-61287b694497' },
+      { type: 'GET',
+        path: [ 'todo', 'a', 'b', 'c' ],
+        id: '737017b2-6d95-4918-a64e-61287b694497' },
+      { type: 'GET',
+        path: [ 'store', 'todos', '1', 'a', 'b', 'c', 'd' ],
+        id: '0fe0b994-693c-40ad-aeda-60f9048a2e03' },
+      { type: 'GET',
+        path: [ 'todo', 'a', 'b', 'c', 'd' ],
+        id: '0fe0b994-693c-40ad-aeda-60f9048a2e03' }
+    ]))
+  })
+
+  const removeDuplicateEvents = events => {
+    return R.reduce((result, event) => {
+      if (result.length === 1 && result[0].id === event.id) {
+        return [event]
+      }
+      if (result.length >= 2 && event.id === R.last(result).id) {
+        if (R.startsWith(R.last(R.init(result)).path, R.last(result).path)) {
+          return result
+        } else {
+          return R.update(result.length - 1, event, result)
+        }
+      }
+      return R.append(event, result)
+    })([], events)
+  }
+
+  test('remove duplicates - access via shorter', () => {
+    const events = [
+      { type: 'GET',
+        path: [ 'store', 'todos', '1', 'a' ],
+        id: 'cdd804c0-49a0-4ca3-8623-5dd2383d337a' },
+      { type: 'GET',
+        path: [ 'todo', 'a' ],
+        id: 'cdd804c0-49a0-4ca3-8623-5dd2383d337a' },
+      { type: 'GET',
+        path: [ 'store', 'todos', '1', 'a', 'b' ],
+        id: '0b0dce92-31a9-40fa-8865-b71d000a05d8' },
+      { type: 'GET',
+        path: [ 'todo', 'a', 'b' ],
+        id: '0b0dce92-31a9-40fa-8865-b71d000a05d8' },
+      { type: 'GET',
+        path: [ 'store', 'todos', '1', 'a', 'b', 'c' ],
+        id: '15de561b-0ec4-448c-825c-d5fb087d8a9b' },
+      { type: 'GET',
+        path: [ 'todo', 'a', 'b', 'c' ],
+        id: '15de561b-0ec4-448c-825c-d5fb087d8a9b' },
+      { type: 'GET',
+        path: [ 'store', 'todos', '1', 'a', 'b', 'c', 'd' ],
+        id: '6ff7380e-1088-4637-b8be-dfe0d1ab5701' },
+      { type: 'GET',
+        path: [ 'todo', 'a', 'b', 'c', 'd' ],
+        id: '6ff7380e-1088-4637-b8be-dfe0d1ab5701' }
+    ]
+    expect(removeDuplicateEvents(events)).toEqual([
+      { type: 'GET',
+        path: [ 'todo', 'a' ],
+        id: 'cdd804c0-49a0-4ca3-8623-5dd2383d337a' },
+      { type: 'GET',
+        path: [ 'todo', 'a', 'b' ],
+        id: '0b0dce92-31a9-40fa-8865-b71d000a05d8' },
+      { type: 'GET',
+        path: [ 'todo', 'a', 'b', 'c' ],
+        id: '15de561b-0ec4-448c-825c-d5fb087d8a9b' },
+      { type: 'GET',
+        path: [ 'todo', 'a', 'b', 'c', 'd' ],
+        id: '6ff7380e-1088-4637-b8be-dfe0d1ab5701' }
+    ])
+  })
+
+  test('remove duplicates - access via longer', () => {
+    const events = [
+      { type: 'GET',
+        path: [ 'store', 'todos' ],
+        id: 'ccd367ca-fd6e-4400-ab3d-039309ca881f' },
+      { type: 'GET',
+        path: [ 'store', 'todos', '1' ],
+        id: 'bf56cbcb-9d56-4f59-9ea3-4cbcca5d0c9f' },
+      { type: 'GET',
+        path: [ 'store', 'todos', '1', 'a' ],
+        id: '0f10e195-14d1-49a0-99f4-5bc5e7a38108' },
+      { type: 'GET',
+        path: [ 'todo', 'a' ],
+        id: '0f10e195-14d1-49a0-99f4-5bc5e7a38108' },
+      { type: 'GET',
+        path: [ 'store', 'todos', '1', 'a', 'b' ],
+        id: '8682c81f-582b-437b-85cd-7fba0bbea26e' },
+      { type: 'GET',
+        path: [ 'todo', 'a', 'b' ],
+        id: '8682c81f-582b-437b-85cd-7fba0bbea26e' },
+      { type: 'GET',
+        path: [ 'store', 'todos', '1', 'a', 'b', 'c' ],
+        id: '737017b2-6d95-4918-a64e-61287b694497' },
+      { type: 'GET',
+        path: [ 'todo', 'a', 'b', 'c' ],
+        id: '737017b2-6d95-4918-a64e-61287b694497' },
+      { type: 'GET',
+        path: [ 'store', 'todos', '1', 'a', 'b', 'c', 'd' ],
+        id: '0fe0b994-693c-40ad-aeda-60f9048a2e03' },
+      { type: 'GET',
+        path: [ 'todo', 'a', 'b', 'c', 'd' ],
+        id: '0fe0b994-693c-40ad-aeda-60f9048a2e03' }
+    ]
+    expect(removeDuplicateEvents(events)).toEqual([
+      { type: 'GET',
+        path: [ 'store', 'todos' ],
+        id: 'ccd367ca-fd6e-4400-ab3d-039309ca881f' },
+      { type: 'GET',
+        path: [ 'store', 'todos', '1' ],
+        id: 'bf56cbcb-9d56-4f59-9ea3-4cbcca5d0c9f' },
+      { type: 'GET',
+        path: [ 'store', 'todos', '1', 'a' ],
+        id: '0f10e195-14d1-49a0-99f4-5bc5e7a38108' },
+      { type: 'GET',
+        path: [ 'store', 'todos', '1', 'a', 'b' ],
+        id: '8682c81f-582b-437b-85cd-7fba0bbea26e' },
+      { type: 'GET',
+        path: [ 'store', 'todos', '1', 'a', 'b', 'c' ],
+        id: '737017b2-6d95-4918-a64e-61287b694497' },
+      { type: 'GET',
+        path: [ 'store', 'todos', '1', 'a', 'b', 'c', 'd' ],
+        id: '0fe0b994-693c-40ad-aeda-60f9048a2e03' }
+    ])
   })
 
   test('delete$', () => {
@@ -56,9 +237,7 @@ describe('duplicate events', () => {
         }
       ]
     })
-
     const todo = store.todos[1]
-
     const props = SubX.create({ store, todo })
     todo.a = { b: { c: { d: 'world' } } }
 
@@ -95,9 +274,7 @@ describe('duplicate events', () => {
         }
       ]
     })
-
     const todo = store.todos[1]
-
     const props = SubX.create({ todo, zzz: store })
 
     const events = []
