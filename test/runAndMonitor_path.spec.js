0e4693be0d92f51cad8375beb8e69883a7b756b0
Tyler Long <tyler4long@gmail.com>
2018-10-16 14:35:52 +0800

    Add test case for runAndMonitor

--- test/runAndMonitor_path.spec.js
+++ test/runAndMonitor_path.spec.js
@@ -14,6 +14,7 @@ describe('runAndMonitor path', () => {
     p.number = 2
     expect(R.map(R.dissoc('id'), events)).toEqual([{ type: 'SET', path: [ 'number' ], val: 2, oldVal: 1 }])
   })
+
   test('root is not subx', () => { // now root must be subx
     const p = SubX.create({
       number: 1
@@ -23,5 +24,41 @@ describe('runAndMonitor path', () => {
     stream.subscribe(e => events.push(e))
     p.number = 2
     expect(R.map(R.dissoc('id'), events)).toEqual([{ type: 'SET', path: [ 'child', 'number' ], val: 2, oldVal: 1 }])
+  })
+
+  test('TodoMVC save', () => {
+    const store = SubX.create({
+      todos: [{
+        title: '111',
+        completed: false
+      }],
+      visibility: 'all'
+    })
+    const stream = SubX.runAndMonitor(store, () => JSON.stringify(store.todos)).stream
+    const events = []
+    stream.subscribe(e => events.push(e))
+    store.todos.push({ title: '222', completed: false })
+    expect(events.length).toBe(1)
+    expect(events[0].path).toEqual(['todos', 'length'])
+  })
+
+  test('toJSON events', () => {
+    const store = SubX.create({
+      todos: [{
+        title: '111',
+        completed: false
+      }],
+      visibility: 'all'
+    })
+    const gets = []
+    const hass = []
+    const keyss = []
+    store.get$.subscribe(e => gets.push(e))
+    store.has$.subscribe(e => hass.push(e))
+    store.keys$.subscribe(e => keyss.push(e))
+    expect(JSON.stringify(store.todos)).toBeDefined()
+    expect(gets.length).toBeGreaterThan(0)
+    expect(hass.length).toBe(0)
+    expect(keyss.length).toBeGreaterThan(0)
   })
 })
