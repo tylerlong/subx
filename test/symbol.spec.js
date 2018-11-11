e7f9fb10d5ba25d08a3287ffa419e85940a61485
Tyler Long <tyler4long@gmail.com>
2018-11-11 13:16:11 +0800

    Add a test case about Symbol

---
+++ test/symbol.spec.js
@@ -0,0 +1,22 @@
+/* eslint-env jest */
+import * as R from 'ramda'
+
+import SubX from '../src/index'
+
+describe('Symbol', () => {
+  test('default', () => {
+    const p = SubX.create()
+    const a = Symbol('a')
+    p[a] = 'hello'
+    const events = []
+    p.$.subscribe(e => events.push(e))
+    p[a] = 'world'
+    expect(R.map(R.dissoc('id'))(events)).toEqual([{ type: 'SET', path: [ a ] }])
+    expect(a).toBe(a)
+    expect(a).toEqual(a)
+  })
+
+  test('symbol and string mixed in path', () => {
+
+  })
+})
