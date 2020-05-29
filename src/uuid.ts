37d57f7e7f118f05e716398baea5634e1baf5eb0
Tyler Long <tyler4long@gmail.com>
2020-05-28 21:51:41 -0700

    Covert uuid.js to ts

--- src/uuid.ts
+++ src/uuid.ts
@@ -1,9 +1,9 @@
 // It's fake uuid, and it is sufficient for this project
 // There is no need for real uuid because it is slow to generate
 
-let id = new Date().valueOf()
+let id = new Date().valueOf();
 const uuid = () => {
-  return ++id
-}
+  return ++id;
+};
 
-export default uuid
+export default uuid;
